9ff838fbd364ca6994deaf798aab8501d201d1ca
Uxair <dotuxair@gmail.com>
2025-09-11 07:54:55 +0500

    removed unused imports and properties

--- Data/Entities/UtmLog.cs
+++ Data/Entities/UtmLog.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using VeloxUrl.Entities;
 
 namespace Velox_Url.Data.Entities
 {
@@ -12,8 +11,6 @@ namespace Velox_Url.Data.Entities
         public string? UtmSource { get; set; }      // e.g., "facebook", "google"
         public string? UtmMedium { get; set; }      // e.g., "cpc", "email"
         public string? UtmCampaign { get; set; }    // e.g., "summer_sale", "launch2025"
-        public string? UtmTerm { get; set; }        // Optional: keyword
-        public string? UtmContent { get; set; }     // Optional: ad variation
 
         [ForeignKey("UrlLog")]
         public int UrlLogId { get; set; }
