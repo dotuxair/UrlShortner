eb4dece86a69975a797363bf0ceae7a7b81d0bba
Uxair <dotuxair@gmail.com>
2025-09-15 22:06:14 +0500

    added data annotations and made columns snake case

--- Data/Entities/UrlLog.cs
+++ Data/Entities/UrlLog.cs
@@ -3,25 +3,40 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Velox_Url.Data.Entities
 {
-    public class UrlLog : BaseEntity
+    public class UrlLog
     {
         [Key]
+        [Column("id")]
         public long Id { get; set; }
 
-        public DateTimeOffset AccessedAt { get; set; } = DateTimeOffset.UtcNow;
-
+        [Column("ip_address")]
         public string? IpAddress { get; set; }
 
-        public string? Device { get; set; }              
-        public string? Browser { get; set; }             
-        public string? OperatingSystem { get; set; }     
-        public string? ReferrerUrl { get; set; }         
+        [Column("device")]
+        public string? Device { get; set; }
 
-        public string? Country { get; set; }             
+        [Column("browser")]
+        public string? Browser { get; set; }
+
+        [Column("operating_system")]
+        public string? OperatingSystem { get; set; }
+
+        [Column("referrer_url")]
+        public string? ReferrerUrl { get; set; }
+
+        [Column("country")]
+        public string? Country { get; set; }
+
+        [Column("city")]
         public string? City { get; set; }
 
-        [ForeignKey("ShortUrl")]
-        public int ShortUrlId { get; set; }
-        public ShortUrl? ShortUrl { get; set; }          
+        [Column("created_at")]
+        public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+
+        [Column("short_url_id")]
+        public long ShortUrlId { get; set; }
+
+        [ForeignKey("ShortUrlId")]
+        public ShortUrl? ShortUrl { get; set; }
     }
 }
--- Data/Entities/UtmLog.cs
+++ Data/Entities/UtmLog.cs
@@ -6,14 +6,22 @@ namespace Velox_Url.Data.Entities
     public class UtmLog
     {
         [Key]
+        [Column("id")]
         public long Id { get; set; }
 
+        [Column("utm_source")]
         public string? UtmSource { get; set; }      // e.g., "facebook", "google"
+
+        [Column("utm_medium")]
         public string? UtmMedium { get; set; }      // e.g., "cpc", "email"
+
+        [Column("utm_campaign")]
         public string? UtmCampaign { get; set; }    // e.g., "summer_sale", "launch2025"
 
-        [ForeignKey("UrlLog")]
-        public int UrlLogId { get; set; }
-        public UrlLog? UrlLog { get; set; }          // Navigation to the access log
+        [Column("url_log_id")]
+        public long UrlLogId { get; set; }
+
+        [ForeignKey(nameof(UrlLogId))]
+        public UrlLog? UrlLog { get; set; }         // Navigation to the access log
     }
 }
