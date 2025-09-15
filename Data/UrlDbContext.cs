f7babb12dee1962fb666de0b1aec7c57c135990c
Uxair <dotuxair@gmail.com>
2025-09-15 22:05:12 +0500

    added Entities in DbContext, while migration they should be used as tables

--- Data/UrlDbContext.cs
+++ Data/UrlDbContext.cs
@@ -1,6 +1,32 @@
-﻿namespace Velox_Url.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Velox_Url.Data.Entities;
+
+namespace Velox_Url.Data
 {
-    public class UrlDbContext
+    public class UrlDbContext(DbContextOptions<UrlDbContext> options) : DbContext(options)
     {
+        public DbSet<ShortUrl> ShortUrls { get; set; }
+        public DbSet<UrlLog> UrlLogs { get; set; }
+        public DbSet<UtmLog> UtmLogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<ShortUrl>()
+                .HasIndex(su => su.ShortUrlCode)
+                .IsUnique();
+
+            modelBuilder.Entity<UrlLog>()
+                .HasOne(ul => ul.ShortUrl)
+                .WithMany()
+                .HasForeignKey(ul => ul.ShortUrlId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<UtmLog>()
+                .HasOne(ul => ul.UrlLog)
+                .WithMany()
+                .HasForeignKey(ul => ul.UrlLogId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
