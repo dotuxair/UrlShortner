f12f5a06f7b9648d8bc81fac4e79bead31c43903
Uxair <dotuxair@gmail.com>
2025-09-15 22:07:13 +0500

    made columns to snake case

--- Data/Entities/BaseEntity.cs
+++ Data/Entities/BaseEntity.cs
@@ -1,10 +1,16 @@
-﻿namespace Velox_Url.Data.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Velox_Url.Data.Entities
 {
     public abstract class BaseEntity
     {
+        [Column("created_at")]
         public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+        [Column("updated_at")]
         public DateTimeOffset? UpdatedAt { get; set; }
+        [Column("is_deleted")]
         public bool IsDeleted { get; set; } = false;
+        [Column("deleted_at")]
         public DateTimeOffset? DeletedAt { get; set; }
     }
 
