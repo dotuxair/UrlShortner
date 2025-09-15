46b7361696fafbabaf6b0752dfe88747e9c958fb
Uxair <dotuxair@gmail.com>
2025-09-15 22:05:59 +0500

    added data annotations and made columns snake case

--- Data/Entities/ShortUrl.cs
+++ Data/Entities/ShortUrl.cs
@@ -1,14 +1,32 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace Velox_Url.Data.Entities
 {
+    [Index(nameof(ShortUrlCode), IsUnique = true)]
     public class ShortUrl : BaseEntity
     {
-        [Key]
         public long Id { get; set; }
+
+        [Required]
+        [Column("original_url")]
         public required string OriginalUrl { get; set; }
+
+        [Required]
+        [Column("short_url_code")]
         public required string ShortUrlCode { get; set; }
-        public int ClickCount { get; set; } = 0;         
+
+        [Column("last_used_at")]
+        public DateTimeOffset? LastUsedAt { get; set; }
+
+        [Column("expired_at")]
+        public DateTimeOffset? ExpiredAt { get; set; }
+
+        [Column("click_count")]
+        public int ClickCount { get; set; } = 0;
+
+        [Column("is_custom")]
         public bool IsCustom { get; set; } = false;
     }
 }
