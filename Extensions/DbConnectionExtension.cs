8f2ab2a4c5a176b1c033f82373eca86226764c03
Uxair <dotuxair@gmail.com>
2025-09-15 22:04:42 +0500

    Extension class to register extensions like services, others etc

---
+++ Extensions/DbConnectionExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Velox_Url.Extensions
+{
+    public static class DbConnectionExtension
+    {
+        public static IServiceCollection AddVeloxUrlDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("VeloxDbConString");
+            services.AddDbContext<Data.UrlDbContext>(options =>
+                options.UseNpgsql(connectionString));
+            return services;
+        }
+    }
+}
