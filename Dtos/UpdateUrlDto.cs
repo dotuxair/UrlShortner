63b7223b47d3aa03cbce6341def0d4281557a9be
Uxair <dotuxair@gmail.com>
2025-09-17 22:29:11 +0500

    Url Update dto.

---
+++ Dtos/UpdateUrlDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Velox_Url.Dtos
+{
+    public record  UpdateUrlDto
+    {
+        [Required]
+        public long UrlId { get; set; }
+        [Required]
+        public required string OriginalUrl { get; set; }
+    }
+}
