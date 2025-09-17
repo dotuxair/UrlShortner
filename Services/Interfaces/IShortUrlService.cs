e17e00e80ee1504e97c33c86a5d6771e51814f6f
Uxair <dotuxair@gmail.com>
2025-09-17 22:30:17 +0500

    ShortUrl interface with method definitions

---
+++ Services/Interfaces/IShortUrlService.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Velox_Url.Data.Entities;
+using Velox_Url.Dtos;
+
+namespace Velox_Url.Services.Interfaces
+{
+    public interface IShortUrlService
+    {
+        Task<ShortUrl?> GetShortUrlByIdAsync(long urlId, CancellationToken cancellationToken);
+
+        Task<ShortUrl?> GetOriginalUrlByShortCodeAsync(string shortCode, CancellationToken cancellationToken);
+
+        Task<ShortUrl> CreateShortUrlAsync(AddShortUrlDto shortUrlDto, CancellationToken cancellationToken);
+
+        Task<ShortUrl?> UpdateShortUrlAsync(UpdateUrlDto updateUrlDto, CancellationToken cancellationToken);
+
+        Task<bool> DeleteShortUrlAsync(long url, CancellationToken cancellationToken);
+
+        Task<bool> TrackShortUrlUsageAsync(long id, CancellationToken cancellationToken);
+    }
+}
