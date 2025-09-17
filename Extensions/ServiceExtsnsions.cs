59e5583da596033357d496342c67f071fe107d13
Uxair <dotuxair@gmail.com>
2025-09-17 22:29:33 +0500

    register shortUrl service, DI feature

---
+++ Extensions/ServiceExtsnsions.cs
@@ -0,0 +1,14 @@
+﻿using Velox_Url.Services;
+using Velox_Url.Services.Interfaces;
+
+namespace Velox_Url.Extensions
+{
+    public static class ServiceExtsnsions
+    {
+        public static IServiceCollection AddVeloxUrlServices(this IServiceCollection services)
+        {
+            services.AddScoped<IShortUrlService, ShortUrlService>();
+            return services;
+        }
+    }
+}
