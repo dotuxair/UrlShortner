59cd4deef61992e595d8993a1b3f11cdd7920ed6
Uxair <dotuxair@gmail.com>
2025-09-17 22:28:40 +0500

    AddShort Url Dto with OriginalUrl and IsCustom property

---
+++ Dtos/AddShortUrlDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Velox_Url.Dtos
+{
+    public record class AddShortUrlDto
+    {
+        [Required]
+        public required string OriginalUrl { get; init; }
+        public bool IsCustom { get; init; } 
+    }
+}
